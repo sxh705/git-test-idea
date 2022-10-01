1872d1d5b6197563db9680f3400cfbd128a84b1c
sxh705 <2362989228@qq.com>
2022-10-01 14:27:35 +0800

    苏欣昊第二次提交

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-
+        var a = "12345";
+        System.out.println("a = " + a);
     }
 }
