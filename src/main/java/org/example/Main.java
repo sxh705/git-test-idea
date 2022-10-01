6e0e4e500a32fddab83cd66008ff8609486e2d4b
sxh705 <2362989228@qq.com>
2022-10-01 14:34:44 +0800

    hot-fix commit 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("a = " + a);
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hot fix test");
     }
 }
