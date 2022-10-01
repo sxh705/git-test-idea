0b4a514859a8d4d43f2a454dbe750819d9b6c52e
sxh705 <2362989228@qq.com>
2022-10-01 14:48:26 +0800

    master commit 2

---
+++ src/main/java/org/example/Main1.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+        var a = "12345";
+        System.out.println("a = " + a);
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("master test");
+        System.out.println("hot fix test");
+    }
+}
